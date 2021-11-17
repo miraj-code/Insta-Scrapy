246564c2bb6a862a31008970cfa4783c7a07ccfe
miraj-code <getanehleul@gmail.com>
2021-11-16 23:10:26 -0800

    First commit

---
+++ scrapy.py
@@ -0,0 +1,58 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+import time
+import os
+import wget
+
+file_path = 'C:/Users/goaim/chromedriver.exe'
+driver = webdriver.Chrome(file_path)
+driver.get("http://www.instagram.com")
+
+username = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, “input[name=’username’]”)))
+password = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, “input[name='password']”)))
+
+username.clear()
+username.send_keys(“my_username”)
+
+password.clear()
+password.send_keys(“my_password”)
+
+Login_button = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, “button[type=’submit’]”))).click()
+
+not_now = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, ‘//button[contains(text(), “Not Now”)]’))).click()
+
+not_now2 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, ‘//button[contains(text(), “Not Now”)]’))).click()
+
+searchbox = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, “//input[@placeholder=’Search’]”)))
+
+searchbox.clear()
+
+keyword = “#motorbike”
+searchbox.send_keys(keyword)
+time.sleep(5)
+searchbox.send_keys(Keys.ENTER)
+time.sleep(5)
+searchbox.send_keys(Keys.ENTER)
+time.sleep(5)
+
+driver.execute_script(“window.scrollTo(0, 4000);”)
+
+images = driver.find_elements_by_tag_name(‘img’)
+images = [image.get_attribute(‘src’) for image in images]
+images = images[:-2]
+
+print(‘Number of scraped images: ‘, len(images))
+
+path = os.getcwd()
+path = os.path.join(path, keyword[1:] + “s”)
+os.mkdir(path)
+
+counter = 0
+for image in images:
+    save_as = os.path.join(path, keyword[1:] + str(counter) +
+    '.jpg')
+    wget.download(image, save_as)
+    counter += 1
